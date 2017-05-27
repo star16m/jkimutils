16b7c1fca632408c7a536d1877cdb93d61cbed3f
star16m <star16m@star16m-PC>
2017-05-27 18:25:16 +0900

    adding web resources.

--- jkimutils/src/main/java/star16m/jkimutils/UtilResource.java
+++ jkimutils/src/main/java/star16m/jkimutils/UtilResource.java
@@ -1,14 +1,26 @@
 package star16m.jkimutils;
 
+import java.io.File;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class UtilResource {
 
+	public static final File PARENT_FILE = new File("webapp");
     @GET
-    public String say(@QueryParam("name") String name) {
-        return "Hello, " + name + "!";
+    @Path("{path:.+}")
+    public Response getPage(@PathParam("path") String path) throws Exception {
+    	System.out.println("calls file[" + path + "]");
+        try {
+        	return Response.ok(new File(PARENT_FILE, path)).build();
+        } catch (Exception e) {
+        	System.err.println("Not found file [" + path + "]");
+        	e.printStackTrace();
+        	return Response.noContent().build();
+        }
     }
 }
