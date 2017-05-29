b6da555eed5c41865a1a77d4715c7749d240dad6
8300741 <8300741@ADK8300600.adk.net>
2017-05-30 08:55:26 +0900

    adding jaxb.

---
+++ jkimutils/src/main/java/star16m/jkimutils/Menu.java
@@ -0,0 +1,25 @@
+package star16m.jkimutils;
+
+public class Menu {
+	
+	private String name;
+	private String link;
+	public Menu(String name, String link) {
+		super();
+		this.name = name;
+		this.link = link;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	
+}
