41e4d65f0b285066163efebb1a39ad4577918b24
8300741 <8300741@ADK8300600.adk.net>
2017-05-31 14:27:50 +0900

    add images directory. refactory MainResource. add jackson lib.

--- jkimutils/src/main/java/star16m/jkimutils/MainResource.java
+++ jkimutils/src/main/java/star16m/jkimutils/MainResource.java
@@ -1,14 +1,16 @@
 package star16m.jkimutils;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 
@@ -16,43 +18,42 @@ import javax.ws.rs.core.UriBuilder;
 public class MainResource {
 
 	public static final File PARENT_FILE = new File("resources");
-	private static String MAIN_FILE = null;
+
 	@GET
 	@Path("/")
 	public Response getMain() throws Exception {
 		return Response.seeOther(UriBuilder.fromUri("main.html").build()).build();
 	}
-    @GET
-    @Path("/{path:.+}")
-    public Response getPage(@PathParam("path") String path) throws Exception {
-    	if (path != null && path.matches("main\\d*.html")) {
-    		String temp = path.replaceAll(".+main", "").replaceAll("\\.html", "");
-//    		if (MAIN_FILE == null) {
-    			synchronized (MainResource.class) {
-					BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File(PARENT_FILE, "main.html")), Charset.forName("UTF-8")));
-					StringBuffer sb = new StringBuffer();
-					String l = null;
-					while ((l = br.readLine()) != null) {
-						sb.append(l);
-					}
-					br.close();
-					MAIN_FILE = sb.toString();
-				}
-//    		}
-    		return Response.ok(MAIN_FILE.replaceAll("#CONTENTS#", temp + "contents")).build();
-    	}
-    	System.out.println("calls file[" + path + "]");
-        try {
-        	return Response.ok(new File(PARENT_FILE, path)).build();
-        } catch (Exception e) {
-        	System.err.println("Not found file [" + path + "]");
-        	e.printStackTrace();
-        	return Response.noContent().build();
-        }
-    }
-    @GET
+
+	@GET
+	@Path("/{path:.+}")
+	public Response getPage(@PathParam("path") String path) throws Exception {
+		System.out.println("calls file[" + path + "]");
+		try {
+			File file = new File(PARENT_FILE, path);
+			if (file.exists()) {
+				return Response.ok(file).build();
+			} else {
+				System.err.println("Not found file [" + path + "]");
+				return Response.status(404).build();
+			}
+		} catch (Exception e) {
+			System.err.println("Not found file [" + path + "]");
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
+	@GET
+	@Path("/menu/list")
+	@Produces("application/json")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public List<Menu> getMenuList() throws Exception {
+		List<Menu> list = Arrays.asList(new Menu("haha", "main.html"), new Menu("aaaa", "main3.html"));
+		return list;
+	}
+	@GET
 	@Path("/webjars/{path:.+}")
-    public Response getWebJars(@PathParam("path") String path) throws Exception {
-    	return Response.ok(getClass().getResourceAsStream("/META-INF/resources/webjars/" + path)).build();
-    }
+	public Response getWebJars(@PathParam("path") String path) throws Exception {
+		return Response.ok(getClass().getResourceAsStream("/META-INF/resources/webjars/" + path)).build();
+	}
 }
